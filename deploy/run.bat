d830ce7b6d8bf2463bf314df6e80d191dcee53b6
Lucas Avila <avila.lucas@colonseguros.com.ar>
2019-09-04 17:20:56 -0300

    se cambian bat

--- deploy/run.bat
+++ deploy/run.bat
@@ -1,20 +1,19 @@
 @ECHO OFF
-
 cd ..
 SET PATH_WORKSPACE=%cd%
 ECHO %PATH_WORKSPACE%
-
-SET PATH_DEPENDENCIES=%cd%/dependencies
-echo %PATH_DEPENDENCIES% 
-
-
+SET PATH_DEPENDENCIES=%cd%\dependencies
+ECHO %PATH_DEPENDENCIES% 
+cd %PATH_WORKSPACE%
 SET AMBIENTE=localhost
 
 docker stop changelog
 docker rm changelog
 
-docker run --name changelog -v %PATH_DEPENDENCIES%:/root/.gradle -v %PATH_WORKSPACE%:/home/workspace --entrypoint="/home/workspace/docker/scripts/dbm-update.sh" changelog %AMBIENTE%
+REM docker run --name changelog -v %PATH_DEPENDENCIES%:/root/.gradle -v %PATH_WORKSPACE%:/home/workspace --entrypoint "bash /home/workspace/docker/scripts/dbm-update-windows.sh" changelog "%AMBIENTE%"
 
-docker rm changelog
+docker run --name changelog -v %PATH_DEPENDENCIES%:/root/.gradle -v %PATH_WORKSPACE%:/home/workspace --entrypoint /home/workspace/docker/scripts/dbm-update-windows.sh changelog "localhost"
+
+REM docker rm changelog
 
 cd deploy
--- docker/scripts/dbm-update-windows.sh
+++ docker/scripts/dbm-update-windows.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
 AMBIENTE=$1
+cd /home/workspace/docker/scripts/
 echo "EJECUTA CHANGELOG EN AMBIENTE: $AMBIENTE"
-
 source ~/.sdkman/bin/sdkman-init.sh
-#export GRAILS_OPTS="-server -Xms1024m -Xmx2048m -XX:-UseGCOverheadLimit -XX:MaxPermSize=1024m -Dfile.encoding=UTF-8"
-cd /home/workspace/
-
+cd ../..
 echo "gradle clean"
 gradle clean
-
 echo "grails dbm-update"
 gradle -Dgrails.env="$AMBIENTE" dbmUpdate
-
 #echo "EJECUTO EL CHANGELOG CON EXITO"
