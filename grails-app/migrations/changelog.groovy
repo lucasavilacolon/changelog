6c5398c6ef56ed1ae5856fe2919ab62ae0f38234
German <german.middi@onmedia.com>
2019-10-07 17:57:46 -0300

    agregamos linea en archivo changelog.groovy

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -1,4 +1,5 @@
 databaseChangeLog = {
 	include file: 'features/create-table-prueba.groovy'
 	include file: 'features/alter-table-prueba.groovy'
+	include file: 'features/alter-table-product-v1.groovy'
 }
