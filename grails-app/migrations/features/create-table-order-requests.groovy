ff8d5e96707cf72b901ebc1c7c5ef7c75bb796af
David <davidmriverog@gmail.com>
2020-06-23 15:01:25 -0300

    correccion

--- grails-app/migrations/features/create-table-order-requests.groovy
+++ grails-app/migrations/features/create-table-order-requests.groovy
@@ -4,7 +4,7 @@ databaseChangeLog = {
 
         grailsChange {
             change {
-                sql.execute("CREATE TABLE `orders_requests` (`order_request_id` int(11) NOT NULL AUTO_INCREMENT, `order_id` int(11) DEFAULT NULL, `endpoint` varchar(255) NOT NULL, `request_payload` json DEFAULT NULL, `response_payload` json DEFAULT NULL, `created_at` datetime NOT NULL, `request_name` varchar(125) DEFAULT NULL, `status_code` varchar(12) NOT NULL, PRIMARY KEY (`order_request_id`), KEY `fk_orderreqidx` (`order_id`)) ENGINE=InnoDB;")
+                sql.execute("CREATE TABLE `orders_requests` (`order_request_id` int(11) NOT NULL AUTO_INCREMENT, `order_id` int(11) DEFAULT NULL, `endpoint` varchar(255) NOT NULL, `request_payload` longtext CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL, `response_payload` longtext CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL, `created_at` datetime NOT NULL, `request_name` varchar(125) DEFAULT NULL, `status_code` varchar(12) NOT NULL, PRIMARY KEY (`order_request_id`), KEY `fk_orderreqidx` (`order_id`)) ENGINE=InnoDB;")
             }
         }
     }
