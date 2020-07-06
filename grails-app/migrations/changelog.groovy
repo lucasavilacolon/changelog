01e66a5eec17cfe10a6c673c3f967ef96db167a5
German <german.middi@gmail.com>
2020-07-06 13:58:33 -0300

    ajustes promociones

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -82,5 +82,6 @@ databaseChangeLog = {
 	include file: 'features/alter-table-orderhdr-5.groovy'
 	include file: 'features/insert-tipo_promocion-1.groovy'
 	include file: 'features/alter-table-promociones-4.groovy'
-	include file: 'features/alter-table-promociones-5.groovy'	
+	include file: 'features/alter-table-promociones-5.groovy'
+	include file: 'features/alter-table-promociones-6.groovy'	
 }
